e0159d6e44b77ab8c678f433badd87af18d5e0ca
PalliPlease <parvjoshi2k4@gmail.com>
2025-07-26 23:49:53 +0530

    updated summary

--- demo.py
+++ demo.py
@@ -1,9 +1,17 @@
 from EDAxe.missing import *
+from EDAxe.summary import *
 import pandas as pd
 
-try:
-    data = pd.read_csv(r"D:\Downloads\202112-divvy-tripdata.csv\202112-divvy-tripdata.csv")
-    # Your dataset here to test
-    print(impute_values(data).isnull().sum())
-except:
-    print("Couldn't find the dataset!")
+#test for missing
+# try:
+data = pd.read_csv(r"D:\Downloads\202112-divvy-tripdata.csv\202112-divvy-tripdata.csv")
+#     # Your dataset here to test
+#     print(impute_values(data).isnull().sum())
+# except:
+#     print("Couldn't find the dataset!")
+
+#test for summary
+# data = pd.read_csv("https://raw.githubusercontent.com/atharvayeola/superstore-analytics-pipeline/master/superstore.csv")
+
+#DONT FORGET TO USE nbformat BEFORE RUNNING THIS!
+generate_summary(data)
