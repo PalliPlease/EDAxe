48831be1142ad393ac264baedbfc6ecd66739549
PalliPlease <parvjoshi2k4@gmail.com>
2025-07-29 13:59:51 +0530

    import updates

--- EDAxe/__init__.py
+++ EDAxe/__init__.py
@@ -1,5 +1,6 @@
 from .preprocessing import impute_values, remove_rows, remove_columns
 from .summary import generate_summary
-from .preprocessing import AutoEncodingPreprocessor
+from .preprocessing import AutoEncodingPreprocessor, AutoScalerPreprocessor
 
-__all__ = ["impute_values", "remove_rows", "remove_columns", "generate_summary", "AutoEncodingPreprocessor"]
+__all__ = ["impute_values", "remove_rows", "remove_columns", "generate_summary", "AutoEncodingPreprocessor",
+           "AutoScalerPreprocessor"]
